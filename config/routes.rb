b8da03096900530d045d815297f7ca786054db91
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-06-15 00:18:15 +0000

    [Start] 3-8

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  get '/', to: 'homes#top'
+  get '/books', to: 'books#index', as: 'books'
+  get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  delete '/books/:id', to: 'books#destroy', as: 'destroy_book'
+  post '/books', to: 'books#create', as: 'create_books'
+  patch '/books/:id', to: 'books#update', as: 'update_book'
+end
